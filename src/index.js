01aabbad03ec7395fca9109c43292561fe0d7c21
elenapoddighe <podd.elena@gmail.com>
2023-12-12 16:06:54 +0100

    Updated context

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateRecipe(event) {
   let instructionsInputElement = document.querySelector("#prompt-instructions");
   let apiKey = "e52093t32d4f473a570bb10aoa2ca4e0";
   let context =
-    "You are a recipe expert and love to share recipes about worldwide food. Your mission is to generate easy recipes in basic HTML. Sign the recipe with 'SheCodes AI'. 'SheCodes AI' must be inside a <strong> element at the bottom of the recipe. Do include a title to the poem but make its color black. Make sure to follow the user instructions.";
+    "You are a recipe expert and love to share recipes about worldwide food. Your mission is to generate easy recipes in basic HTML. Sign the recipe with 'SheCodes AI'. 'SheCodes AI' must be inside a <strong> element at the bottom of the recipe and NOT at the beginning. Do include a black title to the recipe. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a recipe about ${instructionsInputElement.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
